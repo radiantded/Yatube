fe8f4181aca2ae3c03038e2d09d855679b8df212
Enver <radiantded@gmail.com>
2021-08-23 18:49:57 +0300

    Добавлен шаблон logged_out

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path('logout/', LogoutView.as_view(template_name='users/logged_out.html'),
+    path('logout/', LogoutView.as_view(template_name='templates/logged_out.html'),
       name='logout')
 ]
